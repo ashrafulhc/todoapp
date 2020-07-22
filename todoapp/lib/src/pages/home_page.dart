7bc3e54a43f6c6be21c2ce5ea092fa4f041f9848
ashraful <ashraful.rahi@gmail.com>
2020-07-23 03:13:14 +0600

    Configured appbar with search and popup menu icon

--- todoapp/lib/src/pages/home_page.dart
+++ todoapp/lib/src/pages/home_page.dart
@@ -3,10 +3,41 @@ import 'package:todoapp/src/components/buttons/primary_button.dart';
 import 'package:todoapp/src/services/firebase_auth_service.dart';
 import 'package:todoapp/src/utils/text_styles.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final List<String> choices = ['Search', 'About', 'Logout'];
+
+  void _select(String choice) {
+    print(choice);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: const Text('Categories'),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.search),
+            onPressed: () => _select(choices[0]),
+          ),
+          PopupMenuButton<String>(
+            onSelected: _select,
+            itemBuilder: (BuildContext context) {
+              return choices.skip(1).map((String choice) {
+                return PopupMenuItem<String>(
+                  value: choice,
+                  child: Text(choice),
+                );
+              }).toList();
+            },
+          ),
+        ],
+      ),
       body: Container(
         alignment: Alignment.center,
         child: Column(
